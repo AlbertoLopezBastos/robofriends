8618a267cd6b7690c7cd7efd84c004e33cab36a6
Alberto <elbeto182@hotmail.com>
2020-12-21 12:45:19 -0300

    se quitan tipos de typescript

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 
-const Card = ({ id: any, name: any, email: any }) => {
+const Card = ({ id, name, email }) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src={`https://robohash.org/${id}?size=200x200`} alt="robot" />
